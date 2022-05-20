b787d2d789fb9d93dd10377c99e6b31c0ced4637
Joe Park <sunghun.josephp@gmail.com>
2022-05-20 16:18:46 -0700

    Create Router and Routes for the application

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import './App.css';
+import {Route, Routes} from "react-router-dom"
+
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Nav />
+        <Route path="/" element={<Main />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/stocks/:symbol" element={<Price />}></Route>
+        <Route path="/stocks" element={<Stocks />}></Route>
+      </Routes>
     </div>
   );
 }
