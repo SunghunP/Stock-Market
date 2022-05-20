298c562363cb76fd5bf8f4ed7a9ad4ce923e7de7
Joe Park <sunghun.josephp@gmail.com>
2022-05-20 16:44:50 -0700

    Link component pages to the Nav bar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import './App.css';
-import {Route, Routes} from "react-router-dom"
-
+import {Route, Routes} from "react-router-dom";
+import Nav from './components/Nav';
+import Main from './pages/Main';
+import About from './pages/About';
+import Price from './pages/Price';
+import Stocks from './pages/Stocks';
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <Routes>
-        <Nav />
         <Route path="/" element={<Main />}></Route>
-        <Route path="/about" element={<About />}></Route>
-        <Route path="/stocks/:symbol" element={<Price />}></Route>
-        <Route path="/stocks" element={<Stocks />}></Route>
+        <Route path="/about/" element={<About />}></Route>
+        <Route path="/stocks/:symbol/" element={<Price />}></Route>
+        <Route path="/stocks/" element={<Stocks />}></Route>
       </Routes>
     </div>
   );
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,11 @@
+import { Link } from "react-router-dom"
+
 export default function Nav() {
   return(
-    <div>This is the Nav Bar</div>
+    <div>
+      <Link to='/'>HOME</Link>
+      <Link to='/Stocks'>STOCKS</Link>
+      <Link to='/About'>ABOUT</Link>
+    </div>
   )
 }
