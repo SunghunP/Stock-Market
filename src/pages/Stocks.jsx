c5bfc3d02bd8f1239633fbb87195312a2a17e3bc
Joe Park <sunghun.josephp@gmail.com>
2022-05-20 20:49:15 -0700

    Create Layout for stocks table

--- src/pages/Stocks.jsx
+++ src/pages/Stocks.jsx
@@ -1,3 +1,25 @@
+import stocks from "../models/stocksData"
+import { Link } from "react-router-dom"
+
 export default function Stocks() {
-  return <div>This is the Stocks Page</div>
+  return (
+    <table>
+      <tr>
+        <th>Company</th>
+        <th>Price</th>
+        <th>Gains/Losses</th>
+      </tr>
+      {stocks.map(stock => (
+        <tr>
+          <td><Link to={`/stock/${stock.name}`}>{stock.name}</Link></td>
+          <td>{stock.lastPrice}</td>
+          <td>
+            {(stock.change * stock.open).toFixed(2)}
+            ({stock.change > 0 ? '+': ''}{(stock.change*100).toFixed(1)}%)
+            </td>
+        </tr>
+      ))}
+        
+    </table>
+  )
 }
